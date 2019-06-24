10127faa826a19174100ff12e40e813f22506545
AaLeeyah Housey <aahousey@gmail.com>
2019-06-23 23:52:19 -0400

    HI Park Pages

---
+++ parkalerts/hale/halealerts.js
@@ -0,0 +1,27 @@
+$.getJSON("https://developer.nps.gov/api/v1/alerts?parkCode=hale&api_key=SJ788heSyZSEEK0QdQNGXsKAcq7fLZxDxl4keeYw", 
+    function(info) {
+     console.log(info);
+
+     var alerttitle= info.data[0].title;
+     var alertinfo= info.data[0].description;
+
+     var alerttitle2= info.data[1].title;
+     var alertinfo2= info.data[1].description;
+
+     var alerttitle3= info.data[2].title;
+     var alertinfo3= info.data[2].description;
+
+   
+        
+     $('.alerttitle').append(alerttitle);
+     $('.alertinfo').append(alertinfo);
+
+     $('.alerttitle2').append(alerttitle2);
+     $('.alertinfo2').append(alertinfo2);
+
+     $('.alerttitle3').append(alerttitle3);
+     $('.alertinfo3').append(alertinfo3);
+
+    
+    }
+ );
---
+++ parkvc/hale/halevc.js
@@ -0,0 +1,34 @@
+$.getJSON("https://developer.nps.gov/api/v1/visitorcenters?parkCode=hale&api_key=SJ788heSyZSEEK0QdQNGXsKAcq7fLZxDxl4keeYw", 
+    function(info) {
+     console.log(info);
+
+     var vcname= info.data[0].name
+     var vcdescription= info.data[0].description
+     var vcurl= info.data[0].url
+
+     var vcname2= info.data[1].name
+     var vcdescription2= info.data[1].description
+     var vcurl2= info.data[1].url
+
+     var vcname3= info.data[2].name
+     var vcdescription3= info.data[2].description
+     var vcurl3= info.data[2].url
+
+
+     $('.vcname').append(vcname);
+     $('.vcdescription').append(vcdescription);
+     $('.vcurl').append(vcurl);
+     
+     $('.vcname2').append(vcname2);
+     $('.vcdescription2').append(vcdescription2);
+     $('.vcurl2').append(vcurl2);
+
+     $('.vcname3').append(vcname3);
+     $('.vcdescription3').append(vcdescription3);
+     $('.vcurl3').append(vcurl3);
+
+     
+
+    }
+
+);
